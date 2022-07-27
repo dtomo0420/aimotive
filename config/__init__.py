1c2432f319a666ed8a5dc86631d5dec54681be83
dtomo0420 <dtomo0420@gmail.com>
2022-07-27 14:24:51 +0200

    add database to the project

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,5 @@
 import csv
+import sqlite3
 
 def csv_to_print():
     with open(r'beers.csv', encoding='utf-8') as csvf:
@@ -7,6 +8,14 @@ def csv_to_print():
         for row in data:
             print(row)
 
-if __name__ == '__main__':
+def create_db():
+    connection = sqlite3.connect('../beer.db')
+    cursor = connection.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS BEERS (ABV TEXT, IBU TEXT, ID TEXT, NAME TEXT, STYLE TEXT, BREWERY_ID TEXT, OUNCES TEXT)")
+
+    connection.close()
     csv_to_print()
+
+if __name__ == '__main__':
+    create_db()
     pass
